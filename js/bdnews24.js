4bb5ac4009c1b90980bb657d623080e0d8f0cd8b
Noob-developer-sobur <soburkhan210@gmail.com>
2023-04-15 17:39:43 +0600

    Bnews

---
+++ js/bdnews24.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.image-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        prevArrow: ".s-left",
+        nextArrow: ".s-right",
+      });
+          
+})
